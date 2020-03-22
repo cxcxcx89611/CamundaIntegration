b0f29cd9dd07aecf8db1883e5ed4dea7c025ccbc
xichen@blueplanet.com <xichen@blueplanet.com>
2020-03-22 20:11:48 +1100

    first commit for archetype generate Camunda project

---
+++ src/main/java/com/CamundaBpm/CheckWeatherDelegate.java
@@ -0,0 +1,17 @@
+package com.CamundaBpm;
+
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
+
+import java.util.Random;
+
+public class CheckWeatherDelegate implements JavaDelegate {
+
+    @Override
+    public void execute(DelegateExecution delegateExecution) throws Exception {
+        Random random = new Random();
+
+        delegateExecution.setVariable("name", "Niall");
+        delegateExecution.setVariable("weatherOk", random.nextBoolean());
+    }
+}
